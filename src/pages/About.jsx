5f71d2cb5da94d61ee7572ef4418fffee41f2375
yshaaang <erandiotrisha@gmail.com>
2023-02-21 13:56:37 +0800

    Latest update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,13 +7,13 @@ import { GrGlobe } from 'react-icons/gr';
 
 const About = () => {
   return (
-    <section id='ABOUT' className='bg-[#dbf1fe]'>
-      <div className='py-8'>
+    <section id='ABOUT' className='bg-[#dbf1fe] w-full overflow-hidden'>
+      <div className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
         <div
           class={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
         >
           <h2 class='text-2xl font-bold text-gray-900 mb-5'>About Us</h2>
-          <p class='text-poppins text-regular text-black text-justify sm:mr-6 mb-5'>
+          <p class='text-poppins text-regular text-black text-justify mb-5'>
             Welcome to our Ganda Yarn! We're a team of passionate artisans who
             believe that every product should be made with care and attention to
             detail. Our crochet products are crafted using high-quality
@@ -28,110 +28,105 @@ const About = () => {
           </p>
           <div id='about-2'>
             <div className='content-box-lg'>
-              <div class='container'>
-                <div class='row'>
-                  <div class='col-md-4'>
-                    <div class='about-item text-center'>
-                      <GrWorkshop className='icons' />
-                      <h3 class='text-poppins text-regular text-[#3eadcf]'>
-                        MISSION
-                      </h3>
-                      <p className='text-poppins text-regular text-black text-justify'>
-                        {' '}
-                        Ganda Yarn's mission is to bring joy and beauty into the
-                        lives of our customers through our unique and
-                        handcrafted products. We strive to offer a curated
-                        selection of high-quality crochet items that are not
-                        only visually stunning, but also ethically made with
-                        care and attention to detail. We believe that every
-                        customer deserves to experience the warmth and comfort
-                        that comes from owning a handmade crochet item, and we
-                        aim to provide a shopping experience that is both
-                        personal and enjoyable. Our goal is to build a community
-                        of like-minded individuals who appreciate the artistry
-                        and craftsmanship of handmade products, and to support
-                        the livelihoods of our talented artisans who pour their
-                        hearts into each creation.
-                      </p>
-                    </div>
+              <div class='row'>
+                <div class='col-md-4'>
+                  <div class='about-item text-center'>
+                    <GrWorkshop className='icons' />
+                    <h3 class='text-poppins text-regular text-[#3eadcf]'>
+                      MISSION
+                    </h3>
+                    <p className='text-poppins text-regular text-black text-justify'>
+                      {' '}
+                      Ganda Yarn's mission is to bring joy and beauty into the
+                      lives of our customers through our unique and handcrafted
+                      products. We strive to offer a curated selection of
+                      high-quality crochet items that are not only visually
+                      stunning, but also ethically made with care and attention
+                      to detail. We believe that every customer deserves to
+                      experience the warmth and comfort that comes from owning a
+                      handmade crochet item, and we aim to provide a shopping
+                      experience that is both personal and enjoyable. Our goal
+                      is to build a community of like-minded individuals who
+                      appreciate the artistry and craftsmanship of handmade
+                      products, and to support the livelihoods of our talented
+                      artisans who pour their hearts into each creation.
+                    </p>
                   </div>
-                  <div class='col-md-4'>
-                    <div class='about-item text-center'>
-                      <GrGlobe className='icons' />
-                      <h3 class='text-poppins text-regular text-[#3eadcf]'>
-                        VISION
-                      </h3>
+                </div>
+                <div class='col-md-4'>
+                  <div class='about-item text-center'>
+                    <GrGlobe className='icons' />
+                    <h3 class='text-poppins text-regular text-[#3eadcf]'>
+                      VISION
+                    </h3>
 
-                      <p className='text-poppins text-regular text-black text-justify'>
-                        {' '}
-                        Ganda Yarn's vision is to become a leading online
-                        destination for unique and high-quality crochet
-                        products, while upholding our commitment to ethical and
-                        sustainable practices. We strive to offer a diverse
-                        range of handmade crochet items that cater to the needs
-                        and preferences of our customers, and to continuously
-                        innovate and improve our product offerings. Our goal is
-                        to foster a community of passionate and creative
-                        individuals who share our love for handmade products and
-                        our values of quality, craftsmanship, and
-                        sustainability. We envision a future where our handmade
-                        crochet products continue to bring joy and beauty into
-                        the lives of our customers, while also making a positive
-                        impact on the world around us.{' '}
-                      </p>
-                    </div>
+                    <p className='text-poppins text-regular text-black text-justify'>
+                      {' '}
+                      Ganda Yarn's vision is to become a leading online
+                      destination for unique and high-quality crochet products,
+                      while upholding our commitment to ethical and sustainable
+                      practices. We strive to offer a diverse range of handmade
+                      crochet items that cater to the needs and preferences of
+                      our customers, and to continuously innovate and improve
+                      our product offerings. Our goal is to foster a community
+                      of passionate and creative individuals who share our love
+                      for handmade products and our values of quality,
+                      craftsmanship, and sustainability. We envision a future
+                      where our handmade crochet products continue to bring joy
+                      and beauty into the lives of our customers, while also
+                      making a positive impact on the world around us.{' '}
+                    </p>
                   </div>
-                  <div class='col-md-4'>
-                    <div class='about-item text-center'>
-                      <h3>
-                        {' '}
-                        <GrAchievement className='icons' />
-                      </h3>
-                      <h3 class='text-poppins text-regular text-[#3eadcf]'>
-                        ACHIEVEMENTS
-                      </h3>
+                </div>
+                <div class='col-md-4'>
+                  <div class='about-item text-center'>
+                    <h3>
+                      {' '}
+                      <GrAchievement className='icons' />
+                    </h3>
+                    <h3 class='text-poppins text-regular text-[#3eadcf]'>
+                      ACHIEVEMENTS
+                    </h3>
 
-                      <p className='text-poppins text-regular text-black text-justify'>
-                        {' '}
-                        As Ganda Yarn's is just a starting business, we are
-                        proud to have already achieved several milestones and
-                        accomplishments. These include:
-                        <p>
-                          1. <b>Building a loyal customer base:</b> We are
-                          grateful to have a growing community of customers who
-                          appreciate the quality and craftsmanship of our
-                          handmade crochet products. We've received positive
-                          feedback and testimonials, which motivates us to
-                          continue providing the best service possible.
-                        </p>
-                        <p>
-                          2. <b>Showcasing unique and high-quality products:</b>{' '}
-                          Our crochet products are made with attention to detail
-                          and are designed to last. We take pride in using
-                          sustainable and eco-friendly materials to ensure that
-                          our products are not only visually stunning but also
-                          responsible.
-                        </p>
-                        <p>
-                          3. <b>Supporting local artisans:</b> We have partnered
-                          with skilled artisans who specialize in handmade
-                          crochet to create our unique products. By
-                          collaborating with these artisans, we support their
-                          livelihoods and help preserve the art of handmade
-                          crochet.
-                        </p>
-                        <p>
-                          4. <b>Continuous growth:</b>We have been expanding our
-                          product range and are continuously seeking new and
-                          innovative ways to bring handmade crochet products to
-                          our customers.
-                        </p>
-                        <br />
-                        We are committed to continuing to achieve and exceed our
-                        goals and look forward to what the future holds for our
-                        handmade crochet ecommerce store.
+                    <p className='text-poppins text-regular text-black text-justify'>
+                      {' '}
+                      As Ganda Yarn's is just a starting business, we are proud
+                      to have already achieved several milestones and
+                      accomplishments. These include:
+                      <p>
+                        1. <b>Building a loyal customer base:</b> We are
+                        grateful to have a growing community of customers who
+                        appreciate the quality and craftsmanship of our handmade
+                        crochet products. We've received positive feedback and
+                        testimonials, which motivates us to continue providing
+                        the best service possible.
                       </p>
-                    </div>
+                      <p>
+                        2. <b>Showcasing unique and high-quality products:</b>{' '}
+                        Our crochet products are made with attention to detail
+                        and are designed to last. We take pride in using
+                        sustainable and eco-friendly materials to ensure that
+                        our products are not only visually stunning but also
+                        responsible.
+                      </p>
+                      <p>
+                        3. <b>Supporting local artisans:</b> We have partnered
+                        with skilled artisans who specialize in handmade crochet
+                        to create our unique products. By collaborating with
+                        these artisans, we support their livelihoods and help
+                        preserve the art of handmade crochet.
+                      </p>
+                      <p>
+                        4. <b>Continuous growth:</b>We have been expanding our
+                        product range and are continuously seeking new and
+                        innovative ways to bring handmade crochet products to
+                        our customers.
+                      </p>
+                      <br />
+                      We are committed to continuing to achieve and exceed our
+                      goals and look forward to what the future holds for our
+                      handmade crochet ecommerce store.
+                    </p>
                   </div>
                 </div>
               </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,55 +4,57 @@ import { FiMapPin, FiPhoneCall, FiMail } from 'react-icons/fi';
 
 const Contact = () => {
   return (
-    <div className='bg-[#dbf1fe]'>
-      <div class='container py-10'>
-        <div class='content'>
-          <div class='left-side'>
-            <div class='address details'>
-              <FiMapPin className='icon1' />
-              <div class='topic'>Address</div>
-              <div class='text-one'>
-                915 Aurora Boulevard Cor. Albany Street, Cubao
+    <div className='bg-[#dbf1fe] w-full overflow-hidden'>
+      <div className='mx-auto'>
+        <div class='container py-8 mx-auto lg:max-w-7xl px-4 sm:px-6 lg:px-20'>
+          <div class='content'>
+            <div class='left-side'>
+              <div class='address details'>
+                <FiMapPin className='icon1' />
+                <div class='topic'>Address</div>
+                <div class='text-one'>
+                  915 Aurora Boulevard Cor. Albany Street, Cubao
+                </div>
+                <div class='text-two'>Quezon City, Metro Manila</div>
               </div>
-              <div class='text-two'>Quezon City, Metro Manila</div>
+              <div class='phone details'>
+                <FiPhoneCall className='icon2' />
+                <div class='topic'>Phone</div>
+                <div class='text-one'> +63(2)9135669</div>
+                <div class='text-two'>+63(2)9121912</div>
+              </div>
+              <div class='email details'>
+                <FiMail className='icon3' />
+                <div class='topic'>Email</div>
+                <div class='text-one'>GandaYarnPh@gmail.com</div>
+                <div class='text-two'>YarnGandaph@gmail.com</div>
+              </div>
             </div>
-            <div class='phone details'>
-              <FiPhoneCall className='icon2' />
-              <div class='topic'>Phone</div>
-              <div class='text-one'> +63(2)9135669</div>
-              <div class='text-two'>+63(2)9121912</div>
+            <div class='right-side'>
+              <div class='topic-text mb-3'>Send us a message</div>
+              <p class='text-[#6b7280] mb-5'>
+                We would love to hear from you! If you have any questions,
+                comments, or feedback, please don't hesitate to contact us. We
+                value your feedback and want to hear from you! Please don't
+                hesitate to reach out to us - we're always here to help.
+              </p>
+              <form action='#'>
+                <div class='input-box border border-[#0ea5e9] rounded-md'>
+                  <input type='text' placeholder='Enter your name'></input>
+                </div>
+                <div class='input-box border border-[#0ea5e9] rounded-md'>
+                  <input type='text' placeholder='Enter your email'></input>
+                </div>
+                <div class='input-box border border-[#0ea5e9] rounded-md'>
+                  <input type='text' placeholder='Enter your message'></input>
+                </div>
+                <div className='mt-10 mb-8'>
+                  <button className='btn btn-secondary btn-wide rounded-full'>
+                    Send Now
+                  </button>
+                </div>
+              </form>
             </div>
-            <div class='email details'>
-              <FiMail className='icon3' />
-              <div class='topic'>Email</div>
-              <div class='text-one'>GandaYarnPh@gmail.com</div>
-              <div class='text-two'>YarnGandaph@gmail.com</div>
-            </div>
-          </div>
-          <div class='right-side'>
-            <div class='topic-text mb-3'>Send us a message</div>
-            <p class='text-[#6b7280] mb-5'>
-              We would love to hear from you! If you have any questions,
-              comments, or feedback, please don't hesitate to contact us. We
-              value your feedback and want to hear from you! Please don't
-              hesitate to reach out to us - we're always here to help.
-            </p>
-            <form action='#'>
-              <div class='input-box border border-[#0ea5e9] rounded-md'>
-                <input type='text' placeholder='Enter your name'></input>
-              </div>
-              <div class='input-box border border-[#0ea5e9] rounded-md'>
-                <input type='text' placeholder='Enter your email'></input>
-              </div>
-              <div class='input-box border border-[#0ea5e9] rounded-md'>
-                <input type='text' placeholder='Enter your message'></input>
-              </div>
-              <div className='mt-10 mb-8'>
-                <button className='btn btn-secondary btn-wide rounded-full'>
-                  Send Now
-                </button>
-              </div>
-            </form>
           </div>
         </div>
       </div>
