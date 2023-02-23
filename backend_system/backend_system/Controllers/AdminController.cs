36123b30fea3031787801f404f437aa0f37691d1
Christian Roed Boyles <flldwthr@gmail.com>
2023-02-23 23:43:32 +0800

    Create backend_system

---
+++ backend_system/backend_system/Controllers/AdminController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using backend_system.Models;
+
+namespace backend_system.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private List<Admin> admins = new List<Admin>
+        {
+            new Admin
+            {
+                ID = 1,
+                Username = "admin",
+                Password = "thisisreallyauniquepassword",
+                FirstName = "Christian Roed",
+                LastName = "Boyles",
+                CreatedAt = DateTime.Now,
+                ModifiedAt = null
+            },
+            new Admin
+            {
+                ID = 2,
+                Username = "chrstnrdbyls",
+                Password = "GNDYRNpakmamsh",
+                FirstName = "Hanni",
+                LastName = "Pham",
+                CreatedAt = DateTime.Now,
+                ModifiedAt = null
+            }
+        };
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<Admin>>> GetAllAdmins()
+        {
+            return Ok(admins);
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Admin>> GetAdmin(int id)
+        {
+            var admin = admins.Find(x => x.ID == id);
+            if(admin is null)
+            {
+                return NotFound("Admin account doesn't exist.");
+            }
+            return Ok(admin);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<Admin>>> AddAdmin([FromBody]Admin admin)
+        {
+            admins.Add(admin);
+            return Ok(admins);
+        }
+    }
+}
---
+++ backend_system/backend_system/Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿namespace backend_system.Models
+{
+    public class Admin
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
